f6e0a0e90f9fae62247e7da1bd6c649e4672986d
潘晨豪 <632344940@qq.com>
2025-02-25 21:50:44 +0800

    4.1通过三角形面片填充模型，根据重心坐标，如果点在三角形内部，就绘制

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include<math.h>//abs和swap函数
 #include<geometry.h>//几何库，Vec3f这些
 #include<model.h>//读取模型obj的库
-
+#include<random>
 
 const TGAColor black = TGAColor(0, 0, 0, 255);
 const TGAColor white = TGAColor(255, 255, 255, 255);
@@ -46,44 +46,33 @@ void drawLine(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
 	}
 	*/
 
-
 	//Bresenham算法，只有整数，性能更好
 	int dx = x1 - x0;
 	int dy = y1 - y0;
 	int error = abs(dy) * 2;//×2是因为要四舍五入取整
 	int Sumerror = 0;//累计差值
-
 	int  cur_y = y0;
 	for (int cur_x = x0; cur_x < x1; cur_x++)
 	{
 		if (steep) {
 			image.set(cur_y, cur_x, color);
 		}
-		else { image.set(cur_x, cur_y, color); }
-
+		else { 
+			image.set(cur_x, cur_y, color); 
+		}
 		Sumerror += error;
 		if (Sumerror > dx) {
 			cur_y += (y1 > y0 ? 1 : -1);
 			Sumerror -= (dx * 2);
 		}
 	}
-
 }
 
-int main(int argc, char** argv) {
-
-	int imageHeight = 1000;
-	int imageWidth = 1000;
-
-	TGAImage image(imageWidth, imageHeight, TGAImage::RGB);
-
-	//读取模型
-	Model* model = new Model("model/african_head.obj");
-
+//通过线段绘制出模型的轮廓
+void drawModelLine(Model *model,TGAImage& image) {//一个是指针*,一个是&引用
 	for (int i = 0; i < model->nfaces(); i++)
 	{
 		std::vector<int> cur_face = model->face(i);//当前面片
-
 		for (int j = 0; j < 3; j++)
 		{
 			//取当前面片的其中两个点进行绘制线段
@@ -91,20 +80,89 @@ int main(int argc, char** argv) {
 			Vec3f v1 = model->vert(cur_face[(j + 1) % 3]);
 
 			//得到两个点的横纵坐标，至于为什么+1/2，是我们需要把.obj文件中的数据范围[-1,1]->当前image的范围
-			int x0 = (1 + v0.x) * imageWidth / 2;
-			int y0 = (1 + v0.y) * imageHeight / 2;
-			int x1 = (1 + v1.x) * imageWidth / 2;
-			int y1 = (1 + v1.y) * imageHeight / 2;
-
-
+			int x0 = (1 + v0.x) * image.get_width() / 2;
+			int y0 = (1 + v0.y) * image.get_height() / 2;
+			int x1 = (1 + v1.x) * image.get_width() / 2;
+			int y1 = (1 + v1.y) * image.get_height() / 2;
 			drawLine(x0, y0, x1, y1, image, white);
 		}
 	}
+}
 
+//求出三角形的重心坐标,根据公式uAB+vAC+PA=0来求重心坐标
+Vec3f barycentric(Vec2f AB, Vec2f AC, Vec2f PA) {
+	Vec3f bary;
+	bary = Vec3f{ AB.x,AC.x,PA.x } ^ Vec3f{ AB.y,AC.y,PA.y };//做叉乘，得到uv1
+	if (abs(bary.z) < 1e-6) {//bary.z代表三角形面积的两倍，当bary.z<1e-6时，说明这个三角形已经退化，几乎成一条直线
+		return Vec3f{ -1,1,1 };
+	}
+	//返回重心坐标
+	 return Vec3f(1.f - (bary.x + bary.y) / bary.z, bary.x / bary.z, bary.y / bary.z);
+}
+
+//通过三角形面板绘制出模型
+void drawModelTriangle(Model* model, TGAImage& image) {
+
+	for (int i = 0; i < model->nfaces(); i++)
+	{
+		std::vector<int> cur_face = model->face(i);
+		std::vector<Vec3f> cur_v;
+		
+		for (int j = 0; j < 3; j++)
+		{
+			cur_v.push_back(model->vert(cur_face[j]));
+			//将.obj的数据范围[-1,1]转化到该图像的范围
+				cur_v[j].x = ((cur_v[j].x + 1) / 2 )* image.get_width();
+				cur_v[j].y = ((cur_v[j].y + 1) / 2 )* image.get_height();
+		}
+
+		//三角形包围盒边界
+		float xmin, xmax, ymin, ymax;
+		xmin = std::min({ cur_v[0].x, cur_v[1].x, cur_v[2].x });
+		xmax = std::max({ cur_v[0].x, cur_v[1].x, cur_v[2].x });
+		ymin = std::min({ cur_v[0].y, cur_v[1].y, cur_v[2].y });
+		ymax = std::max({ cur_v[0].y, cur_v[1].y, cur_v[2].y });
+
+		Vec2f AB = Vec2f{ cur_v[1].x - cur_v[0].x ,cur_v[1].y - cur_v[0].y };
+		Vec2f AC = Vec2f{ cur_v[2].x - cur_v[0].x ,cur_v[2].y - cur_v[0].y };
+		
+		TGAColor color = TGAColor(rand()%255, rand() % 255, rand() % 255, 255);
+
+		//遍历包围盒，将在三角形内部的点进行绘制
+		for (int x = xmin; x < xmax; x++)
+		{
+			for (int y = ymin; y < ymax; y++)
+			{
+				Vec2f PA = Vec2f{  cur_v[0].x-x, cur_v[0].y-y };
+				Vec3f bary = barycentric(AB, AC, PA);
+
+				if (bary.x < 0 || bary.y < 0 || bary.z < 0) {
+					continue;
+				}
+				else {
+						image.set(x, y, color);
+				}
+			}
+		}
+	}
+}
+int main(int argc, char** argv) {
+
+	int imageHeight = 1000;
+	int imageWidth = 1000;
+	TGAImage image(imageWidth, imageHeight, TGAImage::RGB);
+
+	//读取模型
+	Model* model = new Model("model/african_head.obj");
+	
+	//通过线段绘制出模型的轮廓
+	//drawModelLine(model,image);
+	//通过三角形面板绘制出模型的轮廓
+	drawModelTriangle(model, image);
 	/*drawLine(13, 20, 80, 40, image, red);
 	drawLine(20, 13, 40, 80, image, green);
 	drawLine(80, 40, 13, 20, image, blue);*/
-	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	image.flip_vertically(); // 上下翻转，因为在image中原点在左上角，二我们看图时原点在左下角
 	image.write_tga_file("output/output.tga");
 	return 0;
 }
