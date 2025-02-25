2804d7649a85c1862bf5afb7ec00561f353800f9
潘晨豪 <632344940@qq.com>
2025-02-26 00:06:43 +0800

    4.2 初步引入光线，与法线点积得到光强来显示模型颜色，并简单整理代码

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include<math.h>//abs和swap函数
 #include<geometry.h>//几何库，Vec3f这些
 #include<model.h>//读取模型obj的库
-#include<random>
+#include<random>//随机函数
 
 const TGAColor black = TGAColor(0, 0, 0, 255);
 const TGAColor white = TGAColor(255, 255, 255, 255);
@@ -27,7 +27,7 @@ void drawLine(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
 		std::swap(y0, y1);
 	}
 	/*
-	* 下面的代码也对，可以正常绘制线段，但是浮点float运算过多，可以全部改为int运算
+	* 下面的代码也对，可以正常绘制线段，但是浮点float运算过多，全部改为int运算
 	float k = abs((y1 - y0) / float(x1 - x0));//float k = abs(y1 - y0) / (x1 - x0);直接这样写会被默认取整
 	float cur_y = y0;
 	float error=0;//误差，原来的取整是整数取整，现在用四舍五入取整，误差>0.5向上取整，<0.5向下取整
@@ -45,7 +45,6 @@ void drawLine(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
 		cur_y += k;
 	}
 	*/
-
 	//Bresenham算法，只有整数，性能更好
 	int dx = x1 - x0;
 	int dy = y1 - y0;
@@ -100,49 +99,65 @@ Vec3f barycentric(Vec2f AB, Vec2f AC, Vec2f PA) {
 	 return Vec3f(1.f - (bary.x + bary.y) / bary.z, bary.x / bary.z, bary.y / bary.z);
 }
 
+//绘制三角形面板
+void drawTriangle(Vec3f *screen_vert,Model* model, TGAImage& image, TGAColor color) {
+
+	//三角形包围盒边界
+	float xmin, xmax, ymin, ymax;
+	xmin = std::min({ screen_vert[0].x, screen_vert[1].x, screen_vert[2].x });
+	xmax = std::max({ screen_vert[0].x, screen_vert[1].x, screen_vert[2].x });
+	ymin = std::min({ screen_vert[0].y, screen_vert[1].y, screen_vert[2].y });
+	ymax = std::max({ screen_vert[0].y, screen_vert[1].y, screen_vert[2].y });
+
+	//AB,AC两条向量用于后面求重心坐标
+	Vec2f AB = Vec2f{ screen_vert[1].x - screen_vert[0].x ,screen_vert[1].y - screen_vert[0].y };
+	Vec2f AC = Vec2f{ screen_vert[2].x - screen_vert[0].x ,screen_vert[2].y - screen_vert[0].y };
+
+	//遍历包围盒，将在三角形内部的点进行绘制
+	for (int x = xmin; x < xmax; x++)
+	{
+		for (int y = ymin; y < ymax; y++)
+		{
+			//根据重心坐标公式得到重心坐标
+			Vec2f PA = Vec2f{ screen_vert[0].x - x, screen_vert[0].y - y };
+			Vec3f bary = barycentric(AB, AC, PA);
+
+			if (bary.x < 0 || bary.y < 0 || bary.z < 0) {
+				continue;
+			}
+			else {
+				image.set(x, y, color);
+			}
+		}
+	}
+}
+
 //通过三角形面板绘制出模型
-void drawModelTriangle(Model* model, TGAImage& image) {
+void drawModel(Model* model, TGAImage& image,Vec3f light_dir) {
 
 	for (int i = 0; i < model->nfaces(); i++)
 	{
 		std::vector<int> cur_face = model->face(i);
-		std::vector<Vec3f> cur_v;
-		
+		Vec3f orignal_vert[3];//原始的点坐标[-1,1]
+		Vec3f screen_vert[3];//转化后的屏幕screen点坐标[0,width],[0,height]
 		for (int j = 0; j < 3; j++)
 		{
-			cur_v.push_back(model->vert(cur_face[j]));
+			orignal_vert[j] = (model->vert(cur_face[j]));
 			//将.obj的数据范围[-1,1]转化到该图像的范围
-				cur_v[j].x = ((cur_v[j].x + 1) / 2 )* image.get_width();
-				cur_v[j].y = ((cur_v[j].y + 1) / 2 )* image.get_height();
+			screen_vert[j].x = ((orignal_vert[j].x + 1) / 2) * image.get_width();
+			screen_vert[j].y = ((orignal_vert[j].y + 1) / 2) * image.get_height();
 		}
 
-		//三角形包围盒边界
-		float xmin, xmax, ymin, ymax;
-		xmin = std::min({ cur_v[0].x, cur_v[1].x, cur_v[2].x });
-		xmax = std::max({ cur_v[0].x, cur_v[1].x, cur_v[2].x });
-		ymin = std::min({ cur_v[0].y, cur_v[1].y, cur_v[2].y });
-		ymax = std::max({ cur_v[0].y, cur_v[1].y, cur_v[2].y });
+	//我猜测，.obj再存储三角形顶点时是按照一定规则的，使得计算法线时都是朝向同一侧，不过也先存疑
+		Vec3f normal = ( orignal_vert[2] - orignal_vert[0] ) ^( orignal_vert[1] - orignal_vert[0] );
+		normal.normalize();//归一化和光线求点积得到光线强度
 
-		Vec2f AB = Vec2f{ cur_v[1].x - cur_v[0].x ,cur_v[1].y - cur_v[0].y };
-		Vec2f AC = Vec2f{ cur_v[2].x - cur_v[0].x ,cur_v[2].y - cur_v[0].y };
-		
-		TGAColor color = TGAColor(rand()%255, rand() % 255, rand() % 255, 255);
+		float intensity = normal * light_dir;//光线强度
 
-		//遍历包围盒，将在三角形内部的点进行绘制
-		for (int x = xmin; x < xmax; x++)
-		{
-			for (int y = ymin; y < ymax; y++)
-			{
-				Vec2f PA = Vec2f{  cur_v[0].x-x, cur_v[0].y-y };
-				Vec3f bary = barycentric(AB, AC, PA);
-
-				if (bary.x < 0 || bary.y < 0 || bary.z < 0) {
-					continue;
-				}
-				else {
-						image.set(x, y, color);
-				}
-			}
+		if (intensity > 0) {
+			//绘制出三角形面板
+			TGAColor color = TGAColor(intensity * 255, intensity * 255, intensity * 255, 255);
+			drawTriangle(screen_vert, model, image, color);
 		}
 	}
 }
@@ -151,14 +166,16 @@ int main(int argc, char** argv) {
 	int imageHeight = 1000;
 	int imageWidth = 1000;
 	TGAImage image(imageWidth, imageHeight, TGAImage::RGB);
-
+	Vec3f light_dir = Vec3f(0, 0, -1);//光照方向
 	//读取模型
 	Model* model = new Model("model/african_head.obj");
-	
+
 	//通过线段绘制出模型的轮廓
 	//drawModelLine(model,image);
-	//通过三角形面板绘制出模型的轮廓
-	drawModelTriangle(model, image);
+	
+	//通过三角形面板绘制出模型
+	drawModel(model, image,light_dir);
+
 	/*drawLine(13, 20, 80, 40, image, red);
 	drawLine(20, 13, 40, 80, image, green);
 	drawLine(80, 40, 13, 20, image, blue);*/
